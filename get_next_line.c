3e68300d2210fec5554e2ac9811115d6be478150
Erdemina <34918271+Erdemina@users.noreply.github.com>
2025-01-10 15:51:19 +0300

    next

---
+++ get_next_line.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eulutas <eulutas@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 15:49:31 by eulutas           #+#    #+#             */
+/*   Updated: 2025/01/10 15:49:31 by eulutas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char *read_line(int fd, char *left)
+{
+	char *buffer;
+	int r_bytes;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (0);
+	r_bytes = 1;
+	while (!finder(left) && r_bytes != 0)
+	{
+		r_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (r_bytes == -1)
+		{
+			free(buffer);
+			free(left);
+			return (0);
+		}
+		buffer[r_bytes] = '\0';
+		left = strjoin(left, buffer);
+	}
+	free(buffer);
+	return (left);
+}
+
+char *get_next_line(int fd)
+{
+	char *line;
+	static char *left;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	left = read_line(fd, left);
+	if (!left)
+		return (0);
+	line = getbefore(left);
+	left = getafter(left);
+	return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eulutas <eulutas@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 15:50:42 by eulutas           #+#    #+#             */
+/*   Updated: 2025/01/10 15:50:42 by eulutas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 1
+#endif
+#include <unistd.h>
+#include <stdlib.h>
+
+char *get_next_line(int fd);
+int ft_strlen(const char *str);
+int finder(const char *str);
+char *strjoin(char *line, char *bf);
+char *getbefore(char *str);
+char *getafter(char *str);
+#endif
